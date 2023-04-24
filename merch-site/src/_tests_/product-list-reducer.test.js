c068183a35a5fca298e68dacc0417969ab5c8836
Zachary <ZackCipolletta@gmail.com>
2023-04-24 11:22:56 -0700

    added test for adding and deleting ticket to list. All tests are passing
    Co-authored-by: James Provance <jb24x7@gmail.com>

--- merch-site/src/_tests_/product-list-reducer.test.js
+++ merch-site/src/_tests_/product-list-reducer.test.js
@@ -1,17 +1,79 @@
 import productListReducer from '../reducers/product-list-reducer';
 
 describe('productListReducer', () => {
-  
+
+  const currentState = {
+    '2123123-21312313-231': {
+      name: 'Ryan & Aimen',
+      price: 2,
+      quantity: 3,
+      imgLink: "",
+      id: '2123123-21312313-231'
+    }, '21asdf23-21312ff13-23d1': {
+      name: 'Shoe',
+      price: 5,
+      quantity: 8,
+      imgLink: "",
+      id: '21asdf23-21312ff13-23d1'
+    }
+  };
+
+  let action;
+  const productData = {
+    name: 'Ryan & Aimen',
+    price: 2,
+    quantity: 3,
+    imgLink: "",
+    id: '2123123-21312313-231'
+  };
 
   test('Should return default state if there is no action type passed into the reducer',
     () => {
       expect(productListReducer({}, { type: null })).toEqual({});
     });
-  
-  
-  
-  
-  
-  
-  
+
+  test('Should successfully add new product data to mainProductList', () => {
+    const { name, price, quantity, imgLink, id } = productData;
+    action = {
+      type: 'ADD_PRODUCT',
+      name: name,
+      price: price,
+      quantity: quantity,
+      imgLink: imgLink,
+      id: id
+    };
+
+    expect(productListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        price: price,
+        quantity: quantity,
+        imgLink: imgLink,
+        id: id
+      }
+    });
+  });
+
+  test('Should successfully delete a product', () => {
+    action = {
+      type: 'DELETE_PRODUCT',
+      id: '2123123-21312313-231'
+    };
+    expect(productListReducer(currentState, action)).toEqual({
+      '21asdf23-21312ff13-23d1': {
+        name: 'Shoe',
+        price: 5,
+        quantity: 8,
+        imgLink: "",
+        id: '21asdf23-21312ff13-23d1'
+      }
+    });
+  });
+
+
+
+
+
+
+
 });
--- merch-site/src/reducers/product-list-reducer.js
+++ merch-site/src/reducers/product-list-reducer.js
@@ -1,6 +1,23 @@
 const reducer = (state = {}, action) => {
-
-  return Object.assign({}, state, {})
+  const { name, price, quantity, imgLink, id } = action;
+  switch (action.type) {
+    case 'ADD_PRODUCT':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          price: price,
+          quantity: quantity,
+          imgLink: imgLink,
+          id: id
+        }
+      });
+    case 'DELETE_PRODUCT':
+      let newState = { ...state };
+      delete newState[id];
+      return newState;
+    default:
+      return state;
+  }
 };
 
 export default reducer;
